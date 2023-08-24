619aac1e9499a1711ce3c05dee0bf524707b61b2
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2023-08-24 18:16:59 +0530

    randomized images

--- src/components/homepage/Images.jsx
+++ src/components/homepage/Images.jsx
@@ -1,9 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import styled from 'styled-components';
 import profile from '../../assets/mainprofile.jpg'
+import profile1 from '../../assets/pr11.png'
+import profile2 from '../../assets/placeholder.png'
+import profile3 from '../../assets/link.png'
+import profile4 from '../../assets/diploma.png'
+const Images = () => {
+	const gallery = [profile, profile1, profile2, profile3, profile4];
+	const [currentImage, setCurrentImage] = useState(gallery[0]);
 
-const MypicDiv = styled.div`
-	background-image: url('${profile}');
+	const handleClick = () => {
+		const randomIndex = Math.floor(Math.random() * gallery.length);
+		setCurrentImage(gallery[randomIndex]);
+	};
+	const MypicDiv = styled.div`
+	background-image: url('${currentImage}');
 	background-color: #0D1117;
 	background-size: cover;
 	background-position: center;
@@ -13,19 +24,20 @@ const MypicDiv = styled.div`
 	margin-top:60px;
 	border-radius:15px;
 	`;
-
-const Images = () => {
 	return (
 		<Fragment>
-			
+
 			<MypicDiv
 				className='d-none d-lg-block left-big-image'
 				id='mypicDiv'
+				onClick={handleClick}
 			>
-				<div id='overlay'></div> 
+				<div id='overlay'></div>
 			</MypicDiv>
 		</Fragment>
 	);
+
 };
+
 
 export default Images;
