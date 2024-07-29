103c19e438a3be265d651957e012e5a9a71bf2e7
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2024-07-30 03:03:15 +0530

    some css tweaks and skill add , resume and fixes alignment !

--- src/components/homepage/Hobbies.jsx
+++ src/components/homepage/Hobbies.jsx
@@ -1,42 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Hobbies = (props) => {
-	const hobbies = props.hobbies;
+  const hobbies = props.hobbies;
 
-	return (
-		<div className='skills'>
-			<div className='title'>Hobbies</div>
-			<div className='row skills'>
-				<div
-					className='col-12'
-					style={{ marginTop: '35px', marginBottom: '15px' }}
-				>
-					{hobbies.map((hobby) => (
-						<Link
-						key={hobby.id}
-							to={import.meta.env.VITE_KEY_KEY + '/memory/' + hobby.title}
-							className='link'
-						>
-							<span
-								className='badge badge-light'
-								style={{
-									fontFamily: 'Open Sans' ,
-									fontWeight: 'bolder',
-									marginRight: '7px',
-									marginBottom: '7px',
-									letterSpacing:'0.1rem',
-									fontSize: '1.0rem',
-								}}
-							>
-								{hobby.title}
-							</span>
-						</Link>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="skills">
+      <div className="title">Hobbies</div>
+      <div className="row skills">
+        <div
+          className="col-12"
+          style={{ marginTop: "35px", marginBottom: "15px" }}
+        >
+          {hobbies.map((hobby) => (
+            <span
+              className="badge badge-light"
+              style={{
+                fontFamily: "Open Sans",
+                // fontWeight: "bolder",
+                marginRight: "7px",
+                marginBottom: "7px",
+                // letterSpacing:'0.1rem',
+                fontSize: "1.0rem",
+              }}
+            >
+              {hobby.title}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hobbies;
--- src/components/homepage/Skills.jsx
+++ src/components/homepage/Skills.jsx
@@ -1,56 +1,53 @@
-import React, { Fragment } from 'react';
-import SkillItem from './SkillItem';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Fragment } from "react";
+import SkillItem from "./SkillItem";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import '../../App.css';
+import "../../App.css";
 
 const Skills = (props) => {
-	const skills = props.skills;
+  const skills = props.skills;
 
-	return (
-		<div className='skills'>
-			<div className='title'>Skills</div>
-			{skills.map((skill) => (
-				
-					<div className='row skills' key={skill.id}>
-						<div
-
-							className='col-12'
-							style={{ marginTop: '35px', marginBottom: '35px' }}
-						>
-							<div
-								className='skillName'
-								style={{
-									color: skill.header.color,
-								}}
-							>
-								{skill.header.name.toUpperCase()}
-							</div>
-							<div
-								className='progress'
-								style={{
-									marginTop: '10px',
-								}}
-							>
-								<div
-									className='progress-bar'
-									style={{
-										width: skill.header.percent + '%',
-										backgroundColor: skill.header.color,
-									}}
-								></div>
-							</div>
-						</div>
-						{skill.skills.map((skill) => (
-							<Fragment key={skill.id}>
-							<SkillItem skill={skill}></SkillItem>
-							</Fragment>
-						))}
-					</div>
-				
-			))}
-		</div>
-	);
+  return (
+    <div className="skills">
+      <div className="title">Skills</div>
+      {skills.map((skill) => (
+        <div className="row skills" key={skill.id}>
+          <div
+            className="col-12"
+            style={{ marginTop: "35px", marginBottom: "35px" }}
+          >
+            <div
+              className="skillName"
+              style={{
+                color: skill.header.color,
+              }}
+            >
+              {skill.header.name}
+            </div>
+            <div
+              className="progress"
+              style={{
+                marginTop: "10px",
+              }}
+            >
+              <div
+                className="progress-bar"
+                style={{
+                  width: skill.header.percent + "%",
+                  backgroundColor: skill.header.color,
+                }}
+              ></div>
+            </div>
+          </div>
+          {skill.skills.map((skill) => (
+            <Fragment key={skill.id}>
+              <SkillItem skill={skill}></SkillItem>
+            </Fragment>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Skills;
