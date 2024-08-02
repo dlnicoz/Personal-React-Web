f8b6c74d8ffcbf5c9242ab3a44721c328f8318e1
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2024-08-02 16:33:40 +0530

    changed into all brighter color

--- src/components/homepage/Images.jsx
+++ src/components/homepage/Images.jsx
@@ -1,42 +1,39 @@
-import React, { Fragment, useState } from 'react';
-import styled from 'styled-components';
-import profile from '../../assets/mainprofile.webp'
-import profile1 from '../../assets/profile1.webp'
-import profile2 from '../../assets/profile2.webp'
-import profile3 from '../../assets/profile3.webp'
+import React, { Fragment, useState } from "react";
+import styled from "styled-components";
+import profile from "../../assets/mainprofile.webp";
+import profile1 from "../../assets/profile1.webp";
+import profile2 from "../../assets/profile2.webp";
+import profile3 from "../../assets/profile3.webp";
 const Images = () => {
-	const gallery = [profile, profile1, profile2, profile3];
-	const [currentImage, setCurrentImage] = useState(gallery[0]);
+  const gallery = [profile, profile1, profile2, profile3];
+  const [currentImage, setCurrentImage] = useState(gallery[3]);
 
-	const handleClick = () => {
-		const randomIndex = Math.floor(Math.random() * gallery.length);
-		setCurrentImage(gallery[randomIndex]);
-	};
-	const MypicDiv = styled.div`
-	background-image: url('${currentImage}');
-	background-color: #0D1117;
-	background-size: cover;
-	background-position: center;
-	width:450px;
-	height:450px;
-	margin-bottom:50px;
-	margin-top:60px;
-	border-radius:15px;
-	`;
-	return (
-		<Fragment>
-
-			<MypicDiv
-				className='d-none d-lg-block left-big-image'
-				id='mypicDiv'
-				onClick={handleClick}
-			>
-				<div id='overlay'></div>
-			</MypicDiv>
-		</Fragment>
-	);
-
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * gallery.length);
+    setCurrentImage(gallery[randomIndex]);
+  };
+  const MypicDiv = styled.div`
+    background-image: url("${currentImage}");
+    background-color: #0d1117;
+    background-size: cover;
+    background-position: center;
+    width: 450px;
+    height: 450px;
+    margin-bottom: 50px;
+    margin-top: 60px;
+    border-radius: 15px;
+  `;
+  return (
+    <Fragment>
+      <MypicDiv
+        className="d-none d-lg-block left-big-image"
+        id="mypicDiv"
+        onClick={handleClick}
+      >
+        <div id="overlay"></div>
+      </MypicDiv>
+    </Fragment>
+  );
 };
-
 
 export default Images;
