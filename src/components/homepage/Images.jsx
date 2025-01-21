e1a6aac2b1bbeefa9ebe0c5a472b3d07259ce081
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2025-01-22 00:52:37 +0530

    updated images link

--- src/components/homepage/Images.jsx
+++ src/components/homepage/Images.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import styled from "styled-components";
 import profile from "../../assets/mainprofile.webp";
 import profile1 from "../../assets/profile1.webp";
@@ -9,6 +9,16 @@ const Images = () => {
   const gallery = [profile, profile1, profile2, profile3];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  // Automatic image change every 7 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % gallery.length);
+    }, 7000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [gallery.length]);
+
+  // Handle image change on click
   const handleClick = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % gallery.length);
   };
@@ -40,7 +50,7 @@ const Images = () => {
       <MypicDiv
         className="d-none d-lg-block left-big-image"
         id="mypicDiv"
-        onClick={handleClick}
+        onClick={handleClick} // Change image on click
       >
         <div id="overlay"></div>
       </MypicDiv>
